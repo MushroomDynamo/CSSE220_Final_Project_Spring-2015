4b95b4fe95906e9462295159c80644e08fa22d99
MushroomDynamo <MushroomDynamo@users.noreply.github.com>
2015-05-05 09:27:40 -0400

    Oops. Forgot to commit Digger source.

--- src/Digger.java
+++ src/Digger.java
@@ -1,4 +1,20 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
-public class Digger {
-	//tests
+import javax.swing.*;
+
+public class Digger extends JFrame {
+
+	public static void main(String args[]) {
+		JFrame gameFrame = new JFrame("Digger");
+		gameFrame.setPreferredSize(new Dimension(640,480));
+		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gameFrame.pack();
+		gameFrame.setLayout(null);
+		
+		gameFrame.setVisible(true);
+//		Graphics gameRenderer = new Graphics2D();
+//		gameFrame.paint(gameRenderer.draw3DRect(64, 64, 64, 64, true));
+	}
 }
