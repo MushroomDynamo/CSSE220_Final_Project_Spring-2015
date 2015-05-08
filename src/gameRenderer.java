b539ff1c3304044c572196742b3ed5568a83b7fc
MushroomDynamo <MushroomDynamo@users.noreply.github.com>
2015-05-07 20:56:09 -0400

    other stuff?

--- src/gameRenderer.java
+++ src/gameRenderer.java
@@ -4,6 +4,7 @@ import java.util.Random;
 import javax.swing.*;
 
 public class gameRenderer extends JPanel {
+	
 	private void doDrawing(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
 		for (int i=0;i<gameGrid.yGrid.size();i++) {
@@ -12,10 +13,17 @@ public class gameRenderer extends JPanel {
 				String objectType = xGrid.get(j).getObjectType();
 				if (objectType == "null") {
 					g2d.setColor(Color.black);
-					g2d.fillRect(i*32, j*32, (i*32)+32, (j*32)+32);
-				} else {
-					g2d.setColor(Color.cyan);
-					g2d.fillRect(i*32, j*32, (i*32)+32, (j*32)+32);
+					g2d.fillRect(j*32, i*32, (j*32)+32, (i*32)+32);
+				} else if (objectType == "ground") {
+					g2d.setColor(Color.orange);
+					g2d.fillRect(j*32, i*32, (j*32)+32, (i*32)+32);
+				} else if (objectType == "emerald") {
+					g2d.setColor(Color.green);
+					g2d.fillRect(j*32, i*32, (j*32)+32, (i*32)+32);
+				} else if (objectType == "hero") {
+					g2d.setColor(Color.magenta);
+					g2d.fillRect(j*32, i*32, (j*32)+32, (i*32)+32);
+					objectHero Hero = new objectHero(j,i);
 				}
 			}
 		}
