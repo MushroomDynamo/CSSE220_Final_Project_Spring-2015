17fb062fdeea7a6906687409a04cddd5cc15a7ca
MushroomDynamo <MushroomDynamo@users.noreply.github.com>
2015-05-08 08:26:58 -0400

    Added grid size fields

--- src/Digger.java
+++ src/Digger.java
@@ -14,15 +14,10 @@ public class Digger extends JFrame {
 	private static String[] levelList = {"test_level.txt","test_level_2.txt"};
 	private static int levelPosition = 0;
 	
+	public static int gameWidth = 20;
+	public static int gameHeight = 15;
+	
 	public Digger() {
-//		Action rightAction = new AbstractAction(){
-//			public void actionPerformed(ActionEvent e) {
-//				System.out.println("dogbagels");
-//				if (Hero.checkForCollisionAtCoordinate(Hero.xPos+1,Hero.yPos) == false) {
-//					Hero.shiftToCoordinate(Hero.xPos+1,Hero.yPos,"hero");
-//				}
-//			}
-//	    };
 
 	    gameRenderer.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0), "up");
 	    gameRenderer.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0), "left");
@@ -92,20 +87,17 @@ public class Digger extends JFrame {
 	}
 
 	public static void main(String args[]) {
-		//JFrame gameFrame = new JFrame("Digger");
 		
 		new Digger();
 		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		gameGrid.instantiateGameGrid(gameFrame,20,15);
 		
-		//gameRenderer gameRenderer = new gameRenderer();
 		gameFrame.getContentPane().add(gameRenderer);
 		gameRenderer.setPreferredSize(new Dimension(640,480));
 		gameFrame.pack();
 		gameFrame.setVisible(true);
 		levelManager.readLevelFile("test_level.txt");
 		
-		//levelManager.readLevelFile("test_level_2.txt");
 		while (true) {
 			gameFrame.repaint();
 			try {
